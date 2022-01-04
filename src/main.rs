1bd9e776c9b14557a86c9c083b11a5548125a06f
Kugeki <rostislav04@list.ru>
2022-01-04 16:17:52 +0300

    add file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("[E] Not enough arguments.");
+    }
+    let filename = &args[1];
+
+    println!("File to read: {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("[E] With file reading.");
+    
+    println!("Book text:\n{}", contents);
 }
